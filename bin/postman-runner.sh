bcc86f4a3fb499e630b02e583b29451b1a7af05a
Chris Cheung <chris@poynt.co>
2018-07-09 17:21:41 -0700

    pom and bin modified

--- bin/postman-runner.sh
+++ bin/postman-runner.sh
@@ -1,2 +1,2 @@
 #/bin/bash!
-java -jar postman-runner-1.0.3-jar-with-dependencies.jar co.poynt.postman.PostmanCollectionRunner $1 $2 $3 $4 $5 $6 $7
+java -jar postman-runner-1.0.3-jar-with-dependencies.jar co.poynt.postman.V1.PostmanCollectionRunnerV1 $1 $2 $3 $4 $5 $6 $7
