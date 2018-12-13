b897bbf0aa0da56ce32210d3b05af0b40d68ae7d
Victor Chau <victor.chau@gmail.com>
2018-12-12 16:41:33 -0800

    Updating unit test

--- src/test/java/co/poynt/postman/test/TestPostman.java
+++ src/test/java/co/poynt/postman/test/TestPostman.java
@@ -1,43 +1,45 @@
 package co.poynt.postman.test;
 
-
-import co.poynt.postman.PostmanReader;
-import co.poynt.postman.PostmanRequestRunner;
-import co.poynt.postman.PostmanRunResult;
-import co.poynt.postman.model.*;
 import org.testng.annotations.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
+import co.poynt.postman.PostmanReader;
+import co.poynt.postman.PostmanRequestRunner;
+import co.poynt.postman.PostmanRunResult;
+import co.poynt.postman.model.PostmanCollection;
+import co.poynt.postman.model.PostmanEnvironment;
+import co.poynt.postman.model.PostmanFolder;
+import co.poynt.postman.model.PostmanItem;
+import co.poynt.postman.model.PostmanVariables;
+
 public class TestPostman {
 
-	@Test(enabled = false)
+	@Test(enabled = true)
 	public void testLoadFile() throws Exception {
 		PostmanRunResult runResult = new PostmanRunResult();
 		ObjectMapper om = new ObjectMapper();
 		om.enable(SerializationFeature.INDENT_OUTPUT);
-		String fileName = "PoyntRegression.postman_collection";
+		String fileName = "PostmanRunnerRegression.postman_collection.json";
 		PostmanReader reader = new PostmanReader();
 
 		PostmanCollection c = reader.readCollectionFileClasspath(fileName);
 		c.init();
 		// System.out.println(om.writeValueAsString(c));
 
-		String envFile = "PoyntRegression.postman_environment";
+		String envFile = "PostmanRunnerRegression.postman_environment.json";
 		PostmanEnvironment e = reader.readEnvironmentFileClasspath(envFile);
 		e.init();
 		// System.out.println(om.writeValueAsString(e));
 
 		PostmanVariables var = new PostmanVariables(e);
 		PostmanRequestRunner runner = new PostmanRequestRunner(var, false);
-		
-		//For each folder, execute each request in order
+
+		// For each folder, execute each request in order
 		for (PostmanFolder f : c.item) {
-			if (f.name.equals("Terminal use-cases")) {
-				for (PostmanItem item : f.item){
-					runner.run(item, runResult);
-				}
+			for (PostmanItem item : f.item) {
+				runner.run(item, runResult);
 			}
 		}
 	}
