627b4e9136eea8a18d19190b190440ce33335345
Victor Chau <victor.chau@gmail.com>
2020-08-14 10:44:37 -0700

    Updated to Jackson 2.11.0

--- src/main/java/co/poynt/postman/Main.java
+++ src/main/java/co/poynt/postman/Main.java
@@ -4,16 +4,16 @@ import picocli.CommandLine;
 
 public class Main {
 	public static void main(String[] args) {
-		PostmanRunnerTools cpaTools = new PostmanRunnerTools();
+		PostmanRunnerTools tools = new PostmanRunnerTools();
 		try {
-			CommandLine cmd = new CommandLine(cpaTools);
+			CommandLine cmd = new CommandLine(tools);
 
 			cmd.parseWithHandlers(
 					new CommandLine.RunLast().useOut(System.out).useAnsi(CommandLine.Help.Ansi.ON),
 					CommandLine.defaultExceptionHandler().useErr(System.err).useAnsi(CommandLine.Help.Ansi.OFF),
 					args);
 		} catch (Exception e) {
-			CommandLine.usage(cpaTools, System.err);
+			CommandLine.usage(tools, System.err);
 		}
 	}
 }
