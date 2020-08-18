62bf6dab8b94e7cbabbb29f0ae101cf22510db2d
Victor Chau <victor.chau@gmail.com>
2020-08-17 17:22:17 -0700

    Added http error handling

--- src/main/java/co/poynt/postman/testrail/PostmanTestrailSyncer.java
+++ src/main/java/co/poynt/postman/testrail/PostmanTestrailSyncer.java
@@ -16,6 +16,7 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
@@ -32,11 +33,14 @@ import picocli.CommandLine;
 public class PostmanTestrailSyncer extends CmdBase implements Runnable {
 	private static final Logger logger = LoggerFactory.getLogger(PostmanTestrailSyncer.class);
 
-	@CommandLine.Option(names = { "-t", "--testrail"}, required = true, description = "Testrail HOSTNAME ONLY. Do not include protocol. i.e. xxxxx.testrail.io")
+	public static final int HTTP_OK = 200;
+
+	@CommandLine.Option(names = { "-t",
+			"--testrail" }, required = true, description = "Testrail HOSTNAME ONLY. Do not include protocol. i.e. xxxxx.testrail.io")
 	private String trHost;
-	
+
 	private String testrailBaseUrl;
-	
+
 	@CommandLine.Option(names = { "-c",
 			"--collection" }, required = true, description = "File name of the POSTMAN collection.")
 	private String colFilename;
@@ -50,9 +54,8 @@ public class PostmanTestrailSyncer extends CmdBase implements Runnable {
 	@CommandLine.Option(names = { "-k", "--api-key" }, required = true, description = "Testrail API key.")
 	private String trApiKey;
 
-
-	//Pattern1: tests["THIS IS A TEST NAME1"] = someExpression;
-	//Pattern2: pm.test("THIS IS A TEST NAME2", function()...;
+	// Pattern1: tests["THIS IS A TEST NAME1"] = someExpression;
+	// Pattern2: pm.test("THIS IS A TEST NAME2", function()...;
 	private static final Pattern TESTNAME_PATTERN1 = Pattern.compile("tests\\[(.*?)\\].*");
 	private static final Pattern TESTNAME_PATTERN2 = Pattern.compile("pm.test\\((.*?),.*");
 
@@ -75,7 +78,7 @@ public class PostmanTestrailSyncer extends CmdBase implements Runnable {
 	@Override
 	public void run() {
 		testrailBaseUrl = TestRailConstants.BASE_URL_PATTERN.replace("{HOSTNAME}", trHost);
-		
+
 		HttpClient httpClient = HttpClients.custom().disableCookieManagement().build();
 		Unirest.setHttpClient(httpClient);
 		PostmanReader reader = new PostmanReader();
@@ -91,10 +94,15 @@ public class PostmanTestrailSyncer extends CmdBase implements Runnable {
 		// First we need to find the suite by matching its name with the postman
 		// collection
 		try {
-			JsonNode response = Unirest.get(testrailBaseUrl + "/get_suites/" + trProjectId)
-					.header("Content-Type", "application/json").basicAuth(trUser, trApiKey).asJson().getBody();
+			HttpResponse<JsonNode> response = Unirest.get(testrailBaseUrl + "/get_suites/" + trProjectId)
+					.header("Content-Type", "application/json").basicAuth(trUser, trApiKey).asJson();
 
-			JSONArray suitesArray = response.getArray();
+			if (response.getStatus() != HTTP_OK) {
+				logger.error("Failed to get suites: {} {}", response.getStatus(), response.getBody());
+				return;
+			}
+
+			JSONArray suitesArray = response.getBody().getArray();
 
 			String suiteName = FilenameUtils.getBaseName(colFilename);
 			JSONObject suite = null;
@@ -121,10 +129,17 @@ public class PostmanTestrailSyncer extends CmdBase implements Runnable {
 	private void syncFolders(List<PostmanFolder> folders, JSONObject suite) {
 		try {
 			String suiteId = String.valueOf(suite.getInt("id"));
-			JsonNode response = Unirest.get(testrailBaseUrl + "/get_sections/" + trProjectId).queryString("suite_id", suiteId)
-					.header("Content-Type", "application/json").basicAuth(trUser, trApiKey).asJson().getBody();
+			HttpResponse<JsonNode> response = Unirest.get(testrailBaseUrl + "/get_sections/" + trProjectId)
+					.queryString("suite_id", suiteId).header("Content-Type", "application/json")
+					.basicAuth(trUser, trApiKey).asJson();
+
+			if (response.getStatus() != HTTP_OK) {
+				logger.error("Failed to get sections: {} {}", response.getStatus(), response.getBody());
+				return;
+			}
+
 			Map<String, JSONObject> allSections = new HashMap<>();
-			JSONArray arraySections = response.getArray();
+			JSONArray arraySections = response.getBody().getArray();
 			for (int i = 0; i < arraySections.length(); i++) {
 				JSONObject section = arraySections.getJSONObject(i);
 				allSections.put(section.getString("name"), section);
@@ -151,10 +166,16 @@ public class PostmanTestrailSyncer extends CmdBase implements Runnable {
 		try {
 			String sectionId = String.valueOf(section.getInt("id"));
 			String suiteId = String.valueOf(section.getInt("suite_id"));
-			JsonNode response = Unirest.get(testrailBaseUrl + "/get_cases/" + trProjectId).queryString("suite_id", suiteId)
-					.queryString("section_id", sectionId).header("Content-Type", "application/json")
-					.basicAuth(trUser, trApiKey).asJson().getBody();
-			JSONArray arrayCases = response.getArray();
+			HttpResponse<JsonNode> response = Unirest.get(testrailBaseUrl + "/get_cases/" + trProjectId)
+					.queryString("suite_id", suiteId).queryString("section_id", sectionId)
+					.header("Content-Type", "application/json").basicAuth(trUser, trApiKey).asJson();
+
+			if (response.getStatus() != HTTP_OK) {
+				logger.error("Failed to get cases: {} {}", response.getStatus(), response.getBody());
+				return;
+			}
+
+			JSONArray arrayCases = response.getBody().getArray();
 			Map<String, JSONObject> allCases = new HashMap<>();
 			for (int i = 0; i < arrayCases.length(); i++) {
 				JSONObject c = arrayCases.getJSONObject(i);
@@ -180,13 +201,13 @@ public class PostmanTestrailSyncer extends CmdBase implements Runnable {
 			logger.error("Failed to sync request.", e);
 		}
 	}
-	
+
 	private List<String> findTests(List<String> script) {
 		List<String> result = new ArrayList<>();
 		for (String line : script) {
 			Matcher matcher1 = TESTNAME_PATTERN1.matcher(line);
 			Matcher matcher2 = TESTNAME_PATTERN2.matcher(line);
-			
+
 			if (matcher1.find(0)) {
 				result.add(matcher1.group(1));
 			} else if (matcher2.find(0)) {
@@ -195,7 +216,7 @@ public class PostmanTestrailSyncer extends CmdBase implements Runnable {
 		}
 		return result;
 	}
-	
+
 	private String buildExpectedResult(PostmanItem item) {
 		String result = "";
 		if (item.event != null) {
@@ -218,12 +239,17 @@ public class PostmanTestrailSyncer extends CmdBase implements Runnable {
 			suite.put("name", suiteName);
 			suite.put("description", suiteName);
 
-			JsonNode response = Unirest.post(testrailBaseUrl + "/add_suite/" + trProjectId)
-					.header("Content-Type", "application/json").basicAuth(trUser, trApiKey).body(suite).asJson()
-					.getBody();
+			HttpResponse<JsonNode> response = Unirest.post(testrailBaseUrl + "/add_suite/" + trProjectId)
+					.header("Content-Type", "application/json").basicAuth(trUser, trApiKey).body(suite).asJson();
 
-			//logger.info(response.toString());
-			return response.getObject();
+			if (response.getStatus() != HTTP_OK) {
+				logger.error("Failed to create suite {}: {} {}", suiteName, response.getStatus(), response.getBody());
+				throw new UnirestException(
+						"Failed to create suite " + suiteName + ": " + response.getStatus() + " " + response.getBody());
+			}
+
+			// logger.info(response.toString());
+			return response.getBody().getObject();
 
 		} catch (UnirestException e) {
 			logger.error("Failed to create suite {}.", suiteName, e);
@@ -238,29 +264,35 @@ public class PostmanTestrailSyncer extends CmdBase implements Runnable {
 			section.put("name", folder.name);
 			section.put("suite_id", suiteId);
 
-			JsonNode response = Unirest.post(testrailBaseUrl + "/add_section/" + trProjectId)
-					.header("Content-Type", "application/json").basicAuth(trUser, trApiKey).body(section).asJson()
-					.getBody();
+			HttpResponse<JsonNode> response = Unirest.post(testrailBaseUrl + "/add_section/" + trProjectId)
+					.header("Content-Type", "application/json").basicAuth(trUser, trApiKey).body(section).asJson();
 
-			//logger.info(response.toString());
-			return response.getObject();
+			if (response.getStatus() != HTTP_OK) {
+				logger.error("Failed to create section {}: {} {}", folder.name, response.getStatus(),
+						response.getBody());
+				throw new UnirestException("Failed to create section " + folder.name + ": " + response.getStatus() + " "
+						+ response.getBody());
+			}
+
+			// logger.info(response.toString());
+			return response.getBody().getObject();
 
 		} catch (UnirestException e) {
 			logger.error("Failed to create suite {}.", folder.name, e);
 			throw new RuntimeException("Failed to create suite.", e);
 		}
 	}
-	
+
 	private String buildStepsDescription(PostmanItem item) {
 		String stepsDescription = item.request.method + " " + item.request.url.raw + "\n";
-		if (item.request.body.raw != null) {
+		if (item.request.body != null && item.request.body.raw != null) {
 			if (item.request.body.raw.startsWith("{")) {
 				try {
 					JSONObject body = new JSONObject(item.request.body.raw);
 					stepsDescription += body.toString(2);
 				} catch (JSONException e) {
-					//logger.error("Failed to prettify raw json: {}", item.request.body.raw, e);
-					//ignore
+					// logger.error("Failed to prettify raw json: {}", item.request.body.raw, e);
+					// ignore
 					stepsDescription += item.request.body.raw;
 				}
 			} else {
@@ -274,19 +306,27 @@ public class PostmanTestrailSyncer extends CmdBase implements Runnable {
 		String caseId = String.valueOf(existingCase.getInt("id"));
 		String stepsDescription = buildStepsDescription(item);
 		String expectedResult = buildExpectedResult(item);
-		
+
 		String existingSteps = existingCase.optString(TestRailConstants.FLD_CUSTOM_STEPS);
 		String existingExpected = existingCase.optString(TestRailConstants.FLD_CUSTOM_EXPECTED);
-		
+
 		if (!stepsDescription.equals(existingSteps) || !expectedResult.equals(existingExpected)) {
 			JSONObject update = new JSONObject();
 			update.put(TestRailConstants.FLD_CUSTOM_STEPS, stepsDescription);
 			update.put(TestRailConstants.FLD_CUSTOM_EXPECTED, expectedResult);
 			try {
 				logger.info("======> Updating existing test case: {}", item.name);
-				JsonNode response = Unirest.post(testrailBaseUrl + "/update_case/" + caseId).header("Content-Type", "application/json").basicAuth(trUser, trApiKey).body(update).asJson().getBody();
-				
-				existingCase = response.getObject();
+				HttpResponse<JsonNode> response = Unirest.post(testrailBaseUrl + "/update_case/" + caseId)
+						.header("Content-Type", "application/json").basicAuth(trUser, trApiKey).body(update).asJson();
+
+				if (response.getStatus() != HTTP_OK) {
+					logger.error("Failed to update case {}: {} {}", item.name, response.getStatus(),
+							response.getBody());
+					throw new UnirestException("Failed to update case " + item.name + ": " + response.getStatus() + " "
+							+ response.getBody());
+				}
+
+				existingCase = response.getBody().getObject();
 			} catch (UnirestException e) {
 				logger.error("Failed to update existing test case: {}", item.name, e);
 				throw new RuntimeException("Failed to update existing test case.", e);
@@ -294,7 +334,7 @@ public class PostmanTestrailSyncer extends CmdBase implements Runnable {
 		}
 		return existingCase;
 	}
-	
+
 	private JSONObject createCase(PostmanItem item, int sectionId) {
 		try {
 			logger.info("======> Creating new case: {}", item.name);
@@ -311,12 +351,17 @@ public class PostmanTestrailSyncer extends CmdBase implements Runnable {
 			trCase.put("custom_envautomated", 9);
 			trCase.put("custom_reviewedby", 1);
 
-			JsonNode response = Unirest.post(testrailBaseUrl + "/add_case/" + String.valueOf(sectionId))
-					.header("Content-Type", "application/json").basicAuth(trUser, trApiKey).body(trCase).asJson()
-					.getBody();
+			HttpResponse<JsonNode> response = Unirest.post(testrailBaseUrl + "/add_case/" + String.valueOf(sectionId))
+					.header("Content-Type", "application/json").basicAuth(trUser, trApiKey).body(trCase).asJson();
 
-			//logger.info(response.toString());
-			return response.getObject();
+			if (response.getStatus() != HTTP_OK) {
+				logger.error("Failed to create case {}: {} {}", item.name, response.getStatus(), response.getBody());
+				throw new UnirestException("Failed to create case {}" + item.name + ": " + response.getStatus() + " "
+						+ response.getBody());
+			}
+
+			// logger.info(response.toString());
+			return response.getBody().getObject();
 
 		} catch (UnirestException e) {
 			logger.error("Failed to create case {}.", item.name);
