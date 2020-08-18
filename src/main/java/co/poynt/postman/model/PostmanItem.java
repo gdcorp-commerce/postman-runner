56c06714bbd7b8ae55e131796bdc5ce5c986f053
Victor Chau <victor.chau@gmail.com>
2020-08-17 20:54:58 -0700

    Removing unused response element from PostmanItem model

--- src/main/java/co/poynt/postman/model/PostmanItem.java
+++ src/main/java/co/poynt/postman/model/PostmanItem.java
@@ -5,6 +5,5 @@ import java.util.List;
 public class PostmanItem {
 	public String name;
 	public List<PostmanEvent> event;
-	public PostmanRequest request;
-	public List<String> response;
+	public PostmanRequest request;	
 }
